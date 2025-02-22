2bf95946bca5956c1acae57b9674ee27b47a860f
Muslim217 <abumuslimsafaraliev3815@gmail.com>
2025-02-22 15:04:12 +0300

    Added application.properties to .gitignore

--- src/main/java/my/app/project/model/Seller.java
+++ src/main/java/my/app/project/model/Seller.java
@@ -11,7 +11,7 @@ public class Seller {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @OneToMany(cascade = CascadeType.PERSIST)
+    @OneToMany
     private List<Product> assortment;
 
 }
